45ec80bf5a1435695d5e679839a74d56af3a4c08
“Diego <diegomaradona778@gmail.com>
2024-06-29 06:56:00 +0300

    change MusicPlayer TestsSpring appCont musicPlayer

--- src/main/Java/ru/myspring/first/TestSpring.java
+++ src/main/Java/ru/myspring/first/TestSpring.java
@@ -2,12 +2,15 @@ package ru.myspring.first;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Music music = context.getBean("musicBean", Music.class);
 
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
         musicPlayer.playMusic();
 
         context.close();
