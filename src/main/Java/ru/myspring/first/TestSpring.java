e8036affe7614d3b0d94019582fcd3a1462c29df
“Diego <diegomaradona778@gmail.com>
2024-07-06 14:20:27 +0300

    Change ClassicalMusic, TestSpring and applicationContext

--- src/main/Java/ru/myspring/first/TestSpring.java
+++ src/main/Java/ru/myspring/first/TestSpring.java
@@ -2,16 +2,30 @@ package ru.myspring.first;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        ClassicalMusic classicalMusic = context.getBean("classicalMusic", ClassicalMusic.class);
+        ClassicalMusic classicalMusic1 = context.getBean("classicalMusic", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
+        System.out.println(classicalMusic1.getSong());
 
-        musicPlayer.playMusic();
+//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        boolean comparision = firstMusicPlayer == secondMusicPlayer;
+//
+//        System.out.println(comparision);
+//
+//        System.out.println(firstMusicPlayer);
+//        System.out.println(secondMusicPlayer);
+//
+//        firstMusicPlayer.setVolume(10);
+//        System.out.println(firstMusicPlayer.getVolume());
+//        System.out.println(secondMusicPlayer.getVolume());
+
+       // musicPlayer.playMusic();
 
         context.close();
     }
